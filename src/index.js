7eeee2562f5ddcd71d7d76498d90de4fbed804f4
Kavishika-Kahandawala <lakshila0000@gmail.com>
2023-09-04 09:39:50 +0530

    fixed vercel analytics

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "./main.css";
-import { vercelAnalytics } from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
 
 // components imports
 import NavBar from "./components/navbar";
@@ -20,4 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-vercelAnalytics();
+inject();
